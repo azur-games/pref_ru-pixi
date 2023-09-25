ebe4cdb13bbc2a2e01ab3e58b237468367fb17a7
pavel.and.ivanov <pavel.and.ivanov@megafon.ru>
2023-09-25 12:06:59 +0300

    add header video

--- src/pages/home_page/Header.tsx
+++ src/pages/home_page/Header.tsx
@@ -1,4 +1,3 @@
-import macbook from 'assets/images/macbook-1.webp';
 import classNames from 'classnames';
 import s from './header/Header.module.scss';
 
@@ -6,11 +5,17 @@ import s from './header/Header.module.scss';
 function Header() {
     return (
         <header className={s._}>
+            <video
+                className={s.video}
+                src="assets/video/PREF_vid002_01_1920х1080_32s_ru.webm"
+                controlsList="nodownload"
+                autoPlay
+                muted
+                loop
+            />
             <div className={classNames(s.container, 'wrapper')}>
                 <h1 className={s.title}>Преферанс: игра в карты онлайн</h1>
-                <a href="/play" className={s.button}>ИГРАТЬ ОНЛАЙН</a>
-
-                <img src={macbook} alt="macbook" className={s.image}/>
+                <a href="/play" className={s.button}><span>ИГРАТЬ ОНЛАЙН</span></a>
             </div>
         </header>
     );
