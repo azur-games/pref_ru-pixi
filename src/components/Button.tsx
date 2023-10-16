5945a3b65385b96ef63159e4bc38866f28b72bce
pavel.and.ivanov <pavel.and.ivanov@megafon.ru>
2023-10-16 12:17:41 +0300

    add Button Link

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,7 @@
 import {ReactComponent as ArrowIcon} from 'assets/icons/arrow_play.svg';
 import cn from "classnames";
 import {MouseEvent, PropsWithChildren} from "react";
+import {Link} from "react-router-dom";
 import s from "./button/Button.module.scss";
 import {ButtonProps} from "./button/ButtonTypes";
 
@@ -11,23 +12,23 @@ function Button({children, href, className, size = 'big', playButton, ...props}:
         if (props.onClick) {
             props.onClick(e);
         }
-        if (href) {
-            window.open(href, '_self');
-        }
     }
 
+    const classNames = cn(s._, className, s[size], playButton && s.play_button);
+
+    const content = <>
+        {children}
+        {playButton && <ArrowIcon/>}
+    </>;
+
     return (
-        <button
-            className={cn(s._, className, s[size], {
-                    [s.play_button]: playButton
-                },
-            )}
-            onClick={onClick}
-            {...props}
-        >
-            {children}
-            {playButton && <ArrowIcon/>}
-        </button>
+        href ?
+            <Link to={href} className={classNames} onClick={onClick}>
+                {content}
+            </Link>
+            : <button className={classNames} onClick={onClick} {...props}>
+                {content}
+            </button>
     );
 }
 
